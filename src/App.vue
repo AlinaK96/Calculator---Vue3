afd5488619bc0c0e2ee0ec1ace64bdda34a7a731
Alina <AliKochneva@yandex.ru>
2023-04-24 13:41:01 +0700

    calculator functions

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+    components: { Calculator },
+    name: 'App',
+    comments: {
+        Calculator
+    }
+}
+</script>
+
+<template>
+<div>
+    <Calculator />
+</div>
+
+</template>
+
+<style>
+body{
+    background: #31475e;
+}
+</style>
